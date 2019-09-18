ea312839931b656092a9868978c5b51abce27bdf
Gustavo Henrique <gu.bat98@gmail.com>
2019-09-18 10:37:24 -0300

    Register resource commands

--- providers/CommandsProvider.js
+++ providers/CommandsProvider.js
@@ -6,12 +6,14 @@ class CommandsProvider extends ServiceProvider {
   register () {
     this.app.bind('Adonis/Commands/cerberus:init', () => require('../commands/init'))
     this.app.bind('Adonis/Commands/cerberus:role', () => require('../commands/Role'))
+    this.app.bind('Adonis/Commands/cerberus:resource', () => require('../commands/Resource'))
   }
 
   boot () {
     const ace = require('@adonisjs/ace')
     ace.addCommand('Adonis/Commands/cerberus:init')
     ace.addCommand('Adonis/Commands/cerberus:role')
+    ace.addCommand('Adonis/Commands/cerberus:resource')
   }
 }
 
