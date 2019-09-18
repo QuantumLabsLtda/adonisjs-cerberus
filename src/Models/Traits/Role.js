a9cb86b6d7ee9859ee5c183aefde58251f8fc394
Gustavo Henrique <gu.bat98@gmail.com>
2019-09-18 16:42:11 -0300

    Knex snakeCaseMappers support

--- src/Models/Traits/Role.js
+++ src/Models/Traits/Role.js
@@ -1,9 +1,15 @@
 'use strict'
+const Config = use('Config')
+const usingSnakeCaseMappers = Config.get('database.usingSnakeCaseMappers')
 
-module.exports = class HasRole {
+module.exports = class Role {
   register (Model) {
     Model.prototype.role = function () {
-      return this.hasOne('Cerberus/Models/Role')
+      if (usingSnakeCaseMappers) {
+        return this.hasOne('Cerberus/Models/Role', 'roleId', 'id')
+      } else {
+        return this.hasOne('Cerberus/Models/Role', 'role_id', 'id')
+      }
     }
   }
 }
