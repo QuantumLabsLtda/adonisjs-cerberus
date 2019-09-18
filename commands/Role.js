e8575508621072092c25239aaa6022e52b8c0469
Gustavo Henrique <gu.bat98@gmail.com>
2019-09-18 10:00:52 -0300

    Create role command

---
+++ commands/Role.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const { Command } = require('@adonisjs/ace')
+const Role = use('Cerberus/Models/Role')
+const Database = use('Database')
+
+class SetupCommand extends Command {
+  /**
+   * The command signature getter to define the
+   * command name, arguments and options.
+   *
+   * @attribute signature
+   * @static
+   *
+   * @return {String}
+   */
+  static get signature () {
+    return 'cerberus:role { name: Name of the role } { slug?: Short name for role }'
+  }
+
+  /**
+   * The command description getter.
+   *
+   * @attribute description
+   * @static
+   *
+   * @return {String}
+   */
+  static get description () {
+    return 'Create new Cerberus Role'
+  }
+
+  /**
+   * The handle method to be executed
+   * when running command
+   *
+   * @method handle
+   *
+   * @param  {Object} args
+   * @param  {Object} options
+   *
+   * @return {void}
+   */
+  async handle ({ name, slug }) {
+    try {
+      // If there's no slug, uses the role name instead
+      if (!slug) slug = name
+
+      await Database.transaction(async (trx) => {
+        // Create the role
+        await Role.create({ name, slug })
+      })
+
+      this.success(`${this.icon('success')} role ${name} created.`)
+    } catch ({ message }) {
+      this.error(message)
+    }
+  }
+}
+
+module.exports = SetupCommand
--- providers/CommandsProvider.js
+++ providers/CommandsProvider.js
@@ -5,11 +5,13 @@ const { ServiceProvider } = require('@adonisjs/fold')
 class CommandsProvider extends ServiceProvider {
   register () {
     this.app.bind('Adonis/Commands/cerberus:init', () => require('../commands/init'))
+    this.app.bind('Adonis/Commands/cerberus:role', () => require('../commands/Role'))
   }
 
   boot () {
     const ace = require('@adonisjs/ace')
     ace.addCommand('Adonis/Commands/cerberus:init')
+    ace.addCommand('Adonis/Commands/cerberus:role')
   }
 }
 
